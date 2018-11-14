2294361cfff027077867b6bf330b88cd4d66042d
waltertan1988 <waltertan1988@163.com>
2018-11-14 19:17:54 +0800

    加入user微服务模块；spring boot版本用2.1.0

---
+++ lychee-springcloud-provider-user/src/main/java/com/walter/lychee/user/provider/BaseUserProvider.java
@@ -0,0 +1,8 @@
+package com.walter.lychee.user.provider;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/provider/user")
+public abstract class BaseUserProvider {
+
+}
