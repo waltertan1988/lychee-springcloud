42cb8202d088387f4d69da12e329d4b2844e0400
waltertan1988 <waltertan1988@163.com>
2018-11-14 23:58:57 +0800

    去掉微服务controller中的RequestMapping

--- lychee-springcloud-api-user-impl/src/main/java/com/walter/lychee/user/api/impl/BaseUserApiImpl.java
+++ lychee-springcloud-api-user-impl/src/main/java/com/walter/lychee/user/api/impl/BaseUserApiImpl.java
@@ -1,9 +1,7 @@
 package com.walter.lychee.user.api.impl;
 
 import org.jboss.logging.Logger;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/api/user")
 public abstract class BaseUserApiImpl {
 
 	protected Logger log = Logger.getLogger(this.getClass());
--- lychee-springcloud-api-user-impl/src/main/java/com/walter/lychee/user/api/impl/UserApiImpl.java
+++ lychee-springcloud-api-user-impl/src/main/java/com/walter/lychee/user/api/impl/UserApiImpl.java
@@ -1,7 +1,6 @@
 package com.walter.lychee.user.api.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +14,6 @@ public class UserApiImpl extends BaseUserApiImpl implements UserApi {
 	@Autowired
 	private SysUserRepository sysUserRepository;
 
-	@GetMapping("/get/{username}")
 	@Override
 	public JpaSysUser getUser(@PathVariable("username") String username) {
 		JpaSysUser user = sysUserRepository.findByUsername(username);
