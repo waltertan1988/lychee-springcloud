2b84a1c25979c13c861d32ed0ff03d3c7533a400
walter.tan <Walter.Tan@infinitus-int.com>
2018-11-28 14:01:01 +0800

    package重命名

--- lychee-springcloud-gateway/src/main/java/com/walter/lychee/gateway/LycheeSpringcloudGatewayApplication.java
+++ lychee-springcloud-gateway/src/main/java/com/walter/lychee/gateway/LycheeSpringcloudGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.walter.lychee.eureka;
+package com.walter.lychee.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
